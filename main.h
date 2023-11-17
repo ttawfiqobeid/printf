2ad46cba0f3c2b25e3e3afebc25b6824217953c1
Pbflex <bellopeter22291@gmail.com>
2023-11-17 04:22:06 -0800

    Made some update

--- main.h
+++ main.h
@@ -67,16 +67,13 @@ int print_hexadecimal(va_list types, char buffer[],
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
-int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 /* Funcion to print memory address */
-int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 /* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
